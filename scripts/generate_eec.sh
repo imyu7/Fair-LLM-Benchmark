ada79efb0d6bf1da6352e8cbd6e9ad496c43461a
imyu7 <nekotrout@gmail.com>
2024-05-25 16:29:17 +0900

    abci用スクリプト追加

--- scripts/generate_eec.sh
+++ scripts/generate_eec.sh
@@ -7,11 +7,14 @@
 #$ -cwd
 
 source /etc/profile.d/modules.sh
-module load python/3.9 cuda/11.8 cudnn/8.6
+module load python/3.10 cuda/11.8 cudnn/8.6
+source /home/$(whoami)/.bashrc
 conda activate bias
 
 huggingface-cli login --token $HF_TOKEN
 export HF_HOME=/scratch/$(whoami)/.cache/huggingface/
 export PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python
+export PYTHONPATH=.
+export OPENAI_API_KEY=$OPENAI_API
 
 python Equity-Evaluation-Corpus/src/generation.py -m $MODEL
